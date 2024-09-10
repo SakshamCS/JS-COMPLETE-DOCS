88dff6ebc8674711067795b152a5f9e95046b455
SakshamCS <sakshamgaur2002jee@gmail.com>
2024-09-11 02:00:42 +0530

    feat:elementSelectors

--- 28_elementSelectors.js
+++ 28_elementSelectors.js
@@ -30,3 +30,48 @@ Array.from(fruits).forEach(fruit => {
 });
 
 // Example 3
+
+const h4Elements = document.getElementsByTagName('h4');
+const liElements = document.getElementsByTagName('li');
+console.log(h4Elements);
+
+// h4elements[0].style.backgroundColor = 'yellow';
+
+// for (let h4Element of h4Elements) {
+//   h4Element.style.backgroundColor = 'yellow';
+// }
+
+// for (let liElement of liElements) {
+//   liElement.style.backgroundColor = 'lightgreen';
+// }
+
+Array.from(h4Elements).forEach(h4Element => {
+  h4Element.style.backgroundColor = 'yellow';
+});
+
+Array.from(liElements).forEach(liElement => {
+  liElement.style.backgroundColor = 'lightgreen';
+});
+
+//Example 4
+
+const element1 = document.querySelector('.fruits');
+//selects the first match
+element1.style.backgroundColor = 'yellow';
+const element2 = document.querySelector('.ol');
+console.log(element2);
+//will return null
+
+//Example 5
+//returns the NodeList
+//similar to html collection but has built-in methods like arrays
+//nodelist are static don't update in dom automatically, html collections are live will update in dom automatically
+
+const foods = document.querySelectorAll('li');
+
+console.log(foods);
+//NodeLists have a built-in foreach method let's use that
+
+foods.forEach(food => {
+  food.style.backgroundColor = 'yellow';
+});
