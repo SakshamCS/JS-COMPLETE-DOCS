cbbfbaf522e3b25bb4aa81919f458ae19008a088
SakshamCS <sakshamgaur2002jee@gmail.com>
2024-09-11 01:23:34 +0530

    element selectors

--- 28_elementSelectors.js
+++ 28_elementSelectors.js
@@ -1,3 +1,32 @@
 // element selectors = Methods used to target and manipulate HTML elements They allow you to select one or multiple HTML elements from the DOM(Document Object Model)
 
-//1. document.getElementById() // ELEMENT OR NULL
+// 1. document.getElementById() // ELEMENT OR NULL
+// 2. document.getElementClassName() // HTML COLLECTION
+// 3. document.getElementByTagName() // HTML COLLECTION
+// 4. document.querySelector() // ELEMENT OR NULL
+// 5. document.querySelectorAll() // NODELIST
+
+// Example 1
+const myHeading = document.getElementById('my-heading');
+
+myHeading.style.backgroundColor = 'yellow';
+myHeading.style.textAlign = 'center';
+
+console.log(myHeading);
+
+// Example 2
+
+const fruits = document.getElementsByClassName('fruits');
+
+console.log(fruits);
+
+// fruits[0].style.backgroundColor = 'yellow';
+// for (let fruit of fruits) {
+//   fruit.style.backgroundColor = 'yellow';
+// }
+
+Array.from(fruits).forEach(fruit => {
+  fruit.style.backgroundColor = 'yellow';
+});
+
+// Example 3
