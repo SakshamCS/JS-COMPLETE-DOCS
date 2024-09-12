88a1f03e7ca584d41aec1789564e8f908699782b
SakshamCS <sakshamgaur2002jee@gmail.com>
2024-09-12 16:01:34 +0530

    fix: closure

--- 23_closure.js
+++ 23_closure.js
@@ -1,4 +1,4 @@
-// closure = A function definted inside of another function,
+// closure = A function defined inside of another function,
 // the inner function has access to the variables
 // and scope of the outer function.
 // Allow for private variables and state maintenance
